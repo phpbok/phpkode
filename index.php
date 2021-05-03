9cd23fc2c2d008c438d4707e043dce355ced18b1
phpbok <83586011+phpbok@users.noreply.github.com>
2021-05-03 14:43:36 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>HTML-side med mange formateringer</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Videoer om HTML, PHP og JavaScript">
+  <meta name="keywords" content="HTML, PHP, JavaScript">
+  <meta name="author" content="Ada Lovelace">
+  <meta http-equiv="refresh" content="10">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Hei du!</h1>
+  <p>Det er hyggelig at du ser innom min HTML-side! Denne teksten er 
+  et eksempel på brødtekst. <br /> 
+  Før i <strong>tiden<strong> ga denne teksten <em>mat på bordet</em> 
+  ettersom skribenter fikk betalt pr. ord. Du må gjerne 
+  <a href="mailto:navn@uia.no">sende meg en e-post</a> om du lurer på 
+  dette.</p>
+  <h2>Dette er en overskrift som er litt mindre enn h1-overskriften</h2>
+  <p>Med en ny overskrift må det nesten følge med litt tekst. Hvis ikke 
+  kan det jo se litt rart ut!</p>
+</body>
+</html>
